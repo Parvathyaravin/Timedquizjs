004496617ff20dce68f9e742b6982d09d6eb5d28
Parvathy Aravind <parvathyajan@gmail.com>
2023-07-27 23:35:16 -0600

    Implemented timed quiz

---
+++ 02-Challenge/Assets/script.js
@@ -0,0 +1,139 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const  statusBox = document.getElementById("status-box")
+const  timerBox =  document.getElementById("timer-container")
+var score=0
+var shuffledQuestions;
+var currentQuestionIndex=0
+
+
+
+var intervalId=0
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  statusBox.innerHTML="!"
+  currentQuestionIndex++
+  setNextQuestion()
+})
+// 
+function startGame() {
+   var start = Date.now();
+   intervalId = setInterval(function() {
+        var delta = Date.now() - start; // milliseconds elapsed since start
+        timerBox.innerHTML="<h3>Timer : "+Math.floor(delta / 1000) +" Seconds</h3>"
+    }, 1000); // update about every second
+
+  score=0
+  statusBox.innerHTML="!"
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(questions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  console.log(question)
+  console.log(question.answers)
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if(correct){
+    statusBox.innerHTML="CORRECT !"
+    score++
+  }else{
+    statusBox.innerHTML="WRONG !"
+  }
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    statusBox.innerHTML = statusBox.innerHTML + "  <h2>  Final Score:  "+ score +"/"+shuffledQuestions.length+"</h2>"
+    startButton.classList.remove('hide')
+    clearInterval(intervalId)
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'Who is the best YouTuber?',
+    answers: [
+      { text: 'Web Dev Simplified', correct: true },
+      { text: 'Traversy Media', correct: true },
+      { text: 'Dev Ed', correct: true },
+      { text: 'Fun Fun Function', correct: true }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
